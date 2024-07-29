9cc4cf67f62f977ce57138bd6996809746b3b8b7
Sergey Chama <sergeychama24@gmail.com>
2024-07-29 15:01:06 +0300

    cleanup fix

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import shows from "./data/shows.js";
 import prepareShowCard from "./features/prepareShowCard.js";
 
 const showList = document.querySelector(".shows__list");
-
 shows.forEach((show) => {
   const card = prepareShowCard(show);
   showList.append(card);
