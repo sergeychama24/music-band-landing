6c9d5544c90cf855ac4ae32bec1dc9567d1da359
Sergey Chama <sergeychama24@gmail.com>
2024-07-29 18:40:56 +0300

    added music section

---
+++ src/data/albums.js
@@ -0,0 +1,77 @@
+import cheshireCat from '../assets/images/albums/1.jpg';
+import dudeRanch from '../assets/images/albums/2.jpg';
+import enema from '../assets/images/albums/3.jpg';
+import toypj from '../assets/images/albums/4.jpg';
+import untitled from '../assets/images/albums/5.jpg';
+import neighborhoods from '../assets/images/albums/6.jpg';
+import california from '../assets/images/albums/7.jpg';
+import nine from '../assets/images/albums/8.jpg';
+import oneMore from '../assets/images/albums/9.jpg';
+
+const albums = [
+  {
+    id: 1,
+    title: 'Cheshire Cat',
+    year: '1995',
+    cover: cheshireCat,
+    link: 'https://open.spotify.com/album/6uTjljlshthbtA2U7V6u83',
+  },
+  {
+    id: 2,
+    title: 'Dude Ranch',
+    year: '1997',
+    cover: dudeRanch,
+    link: 'https://open.spotify.com/album/0RADUK8yTUBidzBke6Eszq',
+  },
+  {
+    id: 3,
+    title: 'Enema of the State',
+    year: '1999',
+    cover: enema,
+    link: 'https://open.spotify.com/album/652N05EcNH1a4bIlUixQE2',
+  },
+  {
+    id: 4,
+    title: 'Take Off Your Pants and Jacket',
+    year: '2001',
+    cover: toypj,
+    link: 'https://open.spotify.com/album/3nHpBmW5wJXGeC3ojBkpey',
+  },
+  {
+    id: 5,
+    title: 'blink-182',
+    year: '2003',
+    cover: untitled,
+    link: 'https://open.spotify.com/album/4hBTxv4QRPePXCFcEI7Vjp',
+  },
+  {
+    id: 6,
+    title: 'Neighborhoods',
+    year: '2011',
+    cover: neighborhoods,
+    link: 'https://open.spotify.com/album/0KjLbdlJYvtzXjGTwPy3lv',
+  },
+  {
+    id: 7,
+    title: 'California',
+    year: '2016',
+    cover: california,
+    link: 'https://open.spotify.com/album/0fmq1b2T3bjRyTsigDWUP6',
+  },
+  {
+    id: 8,
+    title: 'NINE',
+    year: '2019',
+    cover: nine,
+    link: 'https://open.spotify.com/album/4gARZz9eV7zbGbtOjhVTPF',
+  },
+  {
+    id: 9,
+    title: 'One More Time',
+    year: '2023',
+    cover: oneMore,
+    link: 'https://open.spotify.com/album/00txDYFrU4LjWqwKE8iQJA',
+  },
+];
+
+export default albums;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import shows from "./data/shows.js";
+import albums from "./data/albums.js";
+
 import prepareShowCard from "./features/prepareShowCard.js";
 
 const showList = document.querySelector(".shows__list");
@@ -6,3 +8,5 @@ shows.forEach((show) => {
   const card = prepareShowCard(show);
   showList.append(card);
 });
+
+console.log(albums);
