7826111c92048dd288e5cef05f127dd7571b0d2e
Sergey Chama <sergeychama24@gmail.com>
2024-07-29 21:03:37 +0300

    add initialization albums scripts

---
+++ src/features/swiper.js
@@ -0,0 +1,31 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 32,
+    },
+  },
+});
+
+export default swiper;
--- src/main.js
+++ src/main.js
@@ -1,9 +1,40 @@
 import shows from './data/shows.js';
-
+import albums from './data/albums.js';
 import prepareShowCard from './features/prepareShowCard.js';
+import swiper from './features/swiper.js';
+import cheshireCat from './assets/images/albums/1.jpg';
 
 const showList = document.querySelector('.shows__list');
+const musicList = document.querySelector('.music__list');
+
 shows.forEach((show) => {
   const card = prepareShowCard(show);
   showList.append(card);
+});
+
+const prepareMusicCard = (music) => {
+  const musicCardTemplate = document.querySelector(
+    '#music-card-template',
+  ).content;
+
+  const card = musicCardTemplate.querySelector('.music__card').cloneNode(true);
+  const musicImage = card.querySelector('.music__card-image');
+  musicImage.src = music.cover;
+  musicImage.alt = `Cover of ${music.title} album`;
+
+  const musicTitle = card.querySelector('.card__info-heading');
+  musicTitle.textContent = `${music.title}`;
+
+  const musicYear = card.querySelector('.card__info-subheading');
+  musicYear.textContent = `${music.year}`;
+
+  const musicLink = card.querySelector('.music__link');
+  musicLink.href = music.link;
+
+  return card;
+};
+
+albums.forEach((album) => {
+  const card = prepareMusicCard(album);
+  musicList.prepend(card);
 });
